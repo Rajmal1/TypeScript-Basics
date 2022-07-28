cf82f9ee4d628a5075e9f5b8bb5c1a79bfa5c714
Rajmal1 <aeis.gui@hotmail.com>
2022-07-28 19:22:31 -0300

    introducing enum type

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+// const person: {
+//     name: string,
+//     age: number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: 'Guilherme',
+//     age: 22,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Guilherme',
     age: 22,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ['Sports', 'Cooking'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toUpperCase());
+}
+if (person.role = Role.AUTHOR) {
+    console.log('is author');
 }

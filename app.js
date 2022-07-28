a583d52fecb1895011c58ce2b87517bd60f83605
Rajmal1 <aeis.gui@hotmail.com>
2022-07-28 19:10:33 -0300

    introducing array types

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Guilherme',
-    age: 22
+    age: 22,
+    hobbies: ['Sports', 'Cooking']
 };
-console.log(person);
+var favoriteActivities;
+favoriteActivities = ['Sports', 'Cooking'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+const person = {
     name: 'Guilherme',
-    age: 22
+    age: 22,
+    hobbies: ['Sports', 'Cooking']
 };
 
-console.log(person);
+let favoriteActivities: string[];
+favoriteActivities = ['Sports', 'Cooking']
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}

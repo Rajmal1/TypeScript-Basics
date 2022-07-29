2fcb105bbee89cf9442ffe8c1208f3895869f38b
Rajmal1 <aeis.gui@hotmail.com>
2022-07-29 15:35:54 -0300

    introducing function types

--- app.ts
+++ app.ts
@@ -1,28 +1,10 @@
-type Combinable = 'as-number' | 'as-text';
-
-function combine(input1: number | string, input2: number | string, resultType: Combinable) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-
-    return result;
-
-    // if (resultType === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1: number, n2: number): number {
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+    console.log('Result: ', num);
+    return;
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
-
+printResult(add(5, 12));

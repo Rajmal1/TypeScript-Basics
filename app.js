1d706dfb42e237fcdf743c989362ddcfc3d400e1
Rajmal1 <aeis.gui@hotmail.com>
2022-08-03 16:51:21 -0300

    introducing function types

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ function add(n1, n2) {
 }
 function printResult(num) {
     console.log('Result: ', num);
+    return;
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
